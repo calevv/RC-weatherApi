70db8718c15f0edb2e084923f85d5fe319174c87
jeong-minG <ika818488@gmail.com>
2025-04-05 00:02:08 +0900

    Revert "도시명 한글화"
    This reverts commit 3056e19c87f143deaf73365fe06b1b39269fc624.

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export const WeatherBox = ({ weather }) => {
-    const [cityname, setCityname] = useState('');
-    const [loading, setLoading] = useState(true);
-    let { lat, lon } = weather.coord;
-
-    const getLocationName = async (lat, lon) => {
-        setLoading(true);
-        try {
-            let url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&accept-language=ko-kr&lat=${lat}&lon=${lon}`;
-            let response = await fetch(url);
-            let data = await response.json();
-            let address = data.address;
-            console.log(address);
-            setCityname(address);
-            setLoading(false);
-        } catch (err) {
-            console.log(err);
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        getLocationName(lat, lon);
-    }, [lat, lon]);
-
+    //console.log(weather);
     const temp = weather?.main.temp;
     const fahrenheit = (temp * 9) / 5 + 32;
-
     return (
         <div className="weather-box">
-            {loading ? <div className="loading-spinner">Loading...</div> : <h1>{cityname?.city}</h1>}
+            <h1>{weather?.name}</h1>
             <figure>
                 {weather?.weather[0].icon && (
                     <img
